dc5dcbeaee1d54e703e432b5235f5531d5e6cc3f
Arnold <arnold.gonzalez.923@gmail.com>
2021-05-27 13:52:59 -0600

    Added another comment line in the code

--- Project2/bikeshare.py
+++ Project2/bikeshare.py
@@ -4,6 +4,9 @@ import time
 import pandas as pd
 import numpy as np
 
+
+## Databases ## 
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
